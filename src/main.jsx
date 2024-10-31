1f3631f3c5496475583415b80b8138e35448c576
hayyantahirr <hayyantahir7@gmail.com>
2024-10-31 17:32:52 +0500

    installing of libraries

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./screens/Home";
 
-createRoot(document.getElementById('root')).render(
- 
-    <App />
-  
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+
+  <RouterProvider router={router} />
+);
