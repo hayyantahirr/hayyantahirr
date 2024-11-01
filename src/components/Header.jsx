ac57bd35d47760b315de87e80606d78593eed2f6
hayyantahirr <hayyantahir7@gmail.com>
2024-11-02 00:50:42 +0500

    Navbar Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+// styles 
+import "../styles/headers.css";
+//components
+import Navbar from "./Navbar";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b  from-zinc-900 to-zinc-900/0">
+      <div className="max-w-screen-2xl  w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
+        <h1>
+          <a href="/" className="logo">
+            <img
+              src="/images/logo.svg"
+              width="40"
+              height="40"
+              alt="hayyantahirr"
+            />
+          </a>
+        </h1>
+        <div className="relative md:justify-self-center">
+          <button onClick={null} className="menu-btn md:hidden">
+            <span className="material-symbols-rounded">menu</span>
+          </button>
+          <Navbar />
+        </div>
+        <a href="#contact"> Reach Me Out !</a>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+
+const Navbar = () => {
+  const lastActiveLink = useRef();
+  const navItems = [
+    {
+      label: "Home",
+      link: "#home",
+      className: "nav-link active",
+      ref: lastActiveLink,
+    },
+    {
+      label: "About",
+      link: "#about",
+      className: "nav-link",
+    },
+    {
+      label: "Work",
+      link: "#work",
+      className: "nav-link",
+    },
+    {
+      label: "Reviews",
+      link: "#reviews",
+      className: "nav-link",
+    },
+    {
+      label: "Contact",
+      link: "#contact",
+      className: "nav-link md:hidden",
+    },
+  ];
+  return (
+    <nav className="">
+      {navItems.map(({ label, link, className }, index) => (
+        <a
+          key={index}
+          href={link}
+          className={className}
+          onClick={null}
+        >
+          {label}
+        </a>
+      ))}
+    </nav>
+  );
+};
+
+export default Navbar;
