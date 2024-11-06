d6e59d9d7a2a8363541727e82ea5d484b142546f
hayyantahirr <hayyantahir7@gmail.com>
2024-11-06 23:16:32 +0500

    Skill Added to project

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Skill from "../components/Skill";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <main>
         <Hero />
         <About/>
+        <Skill/>
       </main>
     </>
   );
