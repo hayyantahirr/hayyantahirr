c736f2c67c7589e8c2329da9b70e48ab427a8649
hayyantahirr <hayyantahir7@gmail.com>
2024-11-16 21:31:45 +0500

    Project Card work done

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,40 +4,22 @@ import ProjectCards from "./ProjectCards.jsx";
 function Work() {
   const works = [
     {
-      imgSrc: "/images/project-1.jpg",
-      title: "Full stack music app",
-      tags: ["API", "MVC", "Development"],
-      projectLink: "https://musify-5al0.onrender.com/",
+      imgSrc: "/images/Age Calculator.jpg",
+      title: "Functional Age Calculator",
+      tags: ["HTML", "CSS", "JavaScript"],
+      projectLink: "https://age-calculator-virid-iota.vercel.app/",
     },
     {
-      imgSrc: "/images/project-2.jpg",
-      title: "Free stock photo app",
-      tags: ["API", "SPA"],
-      projectLink: "https://pixstock-official.vercel.app/",
+      imgSrc: "/images/MemeVerse.jpg",
+      title: " Responsive Meme Generator",
+      tags: ["Next.js", "TailwindCSS"],
+      projectLink: "https://meme-generator-hazel-nine.vercel.app/",
     },
     {
-      imgSrc: "/images/project-3.jpg",
-      title: "Recipe app",
-      tags: ["Development", "API"],
-      projectLink: "",
-    },
-    {
-      imgSrc: "/images/project-4.jpg",
-      title: "Real state website",
-      tags: ["Web-design", "Development"],
-      projectLink: "https://github.com/codewithsadee-org/wealthome",
-    },
-    {
-      imgSrc: "/images/project-5.jpg",
-      title: "eCommerce website",
-      tags: ["eCommerce", "Development"],
-      projectLink: "https://github.com/codewithsadee/anon-ecommerce-website",
-    },
-    {
-      imgSrc: "/images/project-6.jpg",
-      title: "vCard Personal portfolio",
-      tags: ["Web-design", "Development"],
-      projectLink: "https://github.com/codewithsadee/vcard-personal-portfolio",
+      imgSrc: "/images/React Ecommerce.jpg",
+      title: "Ecommerce App with Redux",
+      tags: ["React.Js", "Node.Js", "MongoDB"],
+      projectLink: "https://react-ecommerce-detail.vercel.app/",
     },
   ];
   return (
@@ -53,7 +35,7 @@ function Work() {
                 tags={item.tags}
                 projectLink={item.projectLink}
                 key={key}
-                classes='reveal-up'
+                classes="reveal-up"
               />
             ))}
           </div>
