827282e8bba4da87a596d7a54e0b3b30eae7913c
hayyantahirr <hayyantahir7@gmail.com>
2025-10-26 21:46:56 +0500

    Lottie added to project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ButtonOutline, ButtonPrimary } from "./Button";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const Hero = () => {
   return (
@@ -41,13 +42,12 @@ const Hero = () => {
             </div>
           </div>
           <div className="hidden lg:block">
-            <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-rose-400 via-25% via-rose-400/40 to-65% rounded-[60px] overflow-hidden">
-              <img
-                src="/images/insta-banner-1.png"
-                alt="Hayyan Tahir"
-                width={656}
-                height={800}
-                className="w-full"
+            <figure className="w-full max-w-[600px] h-[320px]  ml-auto  rounded-[60px] overflow-hidden">
+              <DotLottieReact
+                src="/Developer Blue.lottie"
+                loop
+                autoplay
+                className="w-full h-full"
               />
             </figure>
           </div>
